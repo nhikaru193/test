f433bf8d51ed0ad987dbde2b1f3555f053764148
garigari-hota <daoyuanfanang@gmail.com>
2025-06-03 16:13:22 +0900

    Update sousin2.py

--- sousin2.py
+++ sousin2.py
@@ -1,13 +1,11 @@
 import serial
 import time
 
-# UARTポートとボーレート設定（IM920のデフォルトは19200bps）
 ser = serial.Serial('/dev/serial0', 19200, timeout=1)
 
-# 子機に送るテストデータ
-test_data = "TEST\r\n"
-
+# データを1秒おきに送信
 while True:
-    ser.write(test_data.encode('utf-8'))
-    print("送信：", test_data.strip())
-    time.sleep(5)
+    msg = "Hello from Pi\r"
+    ser.write(msg.encode('utf-8'))
+    print("送信:", msg.strip())
+    time.sleep(1)
