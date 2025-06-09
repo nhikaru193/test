65ee6b58ac2e27384aafce7be01297dec230d0ce
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-09 20:02:14 +0900

    Update motor_call.py

--- motor_call.py
+++ motor_call.py
@@ -2,15 +2,6 @@
 import time
 from motor import MotorDriver
 
-if __name__ == "__main__":
-    # ① MotorDriver のインスタンスを作成（__init__ は自動で呼ばれる）
-    driver = MotorDriver(
-        PWMA=12, AIN1=23, AIN2=18,
-        PWMB=19, BIN1=16, BIN2=26,
-        STBY=21,
-        freq=1000
-    )
-
     # ② メソッドはインスタンス経由で呼び出す
     print("前進加速中")
     driver.changing_forward(0, 80)
