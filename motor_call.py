28106dca08aea92daf4858efe7e0e75994786d84
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-09 20:07:18 +0900

    Update motor_call.py

--- motor_call.py
+++ motor_call.py
@@ -2,19 +2,19 @@
 import time
 from motor import MotorDriver
 
-    # ② メソッドはインスタンス経由で呼び出す
-    print("前進加速中")
-    driver.changing_forward(0, 80)
+# ② メソッドはインスタンス経由で呼び出す
+print("前進加速中")
+driver.changing_forward(0, 80)
 
-    print("前進減速中")
-    driver.changing_forward(80, 0)
+print("前進減速中")
+driver.changing_forward(80, 0)
 
-    print("右折加速中")
-    driver.changing_right(0, 50)
+print("右折加速中")
+driver.changing_right(0, 50)
 
-    print("右折減速中")
-    driver.changing_right(50, 0)
+print("右折減速中")
+driver.changing_right(50, 0)
 
-    # ③ 最後に GPIO をクリーンアップ
-    driver.cleanup()
-  
+# ③ 最後に GPIO をクリーンアップ
+driver.cleanup()
+
