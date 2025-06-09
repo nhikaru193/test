6d4250c4190d3fd07db2a4ede48e0e5a10fe0ee4
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-09 19:51:36 +0900

    Update motor_call.py

--- motor_call.py
+++ motor_call.py
@@ -6,14 +6,14 @@ MotorDriver.__init__(self,
                   PWMB, BIN1, BIN2, STBY,
                   freq = 1000):
 
-print("前進加速中")
-MotorDriver.changing_forward(0, 80)
-
-print("前進減速中")
-MotorDriver.changing_forward(80, 0)
-
-print("右折加速中")
-MotorDriver.changing_right(0, 50)
-
-print("右折減速中")
-MotorDriver.changing_right(50, 0)
+  print("前進加速中")
+  MotorDriver.changing_forward(0, 80)
+  
+  print("前進減速中")
+  MotorDriver.changing_forward(80, 0)
+  
+  print("右折加速中")
+  MotorDriver.changing_right(0, 50)
+  
+  print("右折減速中")
+  MotorDriver.changing_right(50, 0)
