28dab890460cb479d91f37d731dc90f542bcf6cc
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-10 16:25:41 +0900

    Update motor_call.py

--- motor_call.py
+++ motor_call.py
@@ -2,21 +2,18 @@ import RPi.GPIO as GPIO
 from motor import MotorDriver  # motor.py から MotorDriver クラスを読み込む
 import time
 
-# モータードライバのインスタンスを作成（仮のピン番号）
-driver = MotorDriver(
-    PWMA=12, AIN1=23, AIN2=18,
-    PWMB=19, BIN1=16, BIN2=26,
-    STBY=21
-)
+motordriver.__init__(PWMA=12, AIN1=23, AIN2=18,  
+    PWMB=19, BIN1=16, BIN2=26, 
+    STBY=21)
 
 try:
     print("前進（加速）")
-    driver.changing_forward(0, 80)
+    MotorDriver.changing_forward(0, 80)
 
     time.sleep(2)
 
     print("前進（減速）")
-    driver.changing_forward(80, 0)
+    MotorDriver.changing_forward(80, 0)
 
 finally:
     print("停止")
