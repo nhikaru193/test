283d7aba57d2cf611d13e91b14a81c40092691d2
garigari-hota <daoyuanfanang@gmail.com>
2025-06-10 15:18:14 +0900

    Update para_kaihi.py

--- para_kaihi.py
+++ para_kaihi.py
@@ -2,7 +2,8 @@ import time
 import RPi.GPIO as GPIO
 from GNSS_navigate import direction
 from GNSS_navigate import distance
-from motor import motor
+from motor import init
+from motor import right_
 import cv2
 import numpy as np
 from smbus2 import SMBus
