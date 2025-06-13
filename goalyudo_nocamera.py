ab3437bbfd9c730de9700d6b9111fe27858afccc
nhikaru193 <nakagawa@kimura-lab.net>
2025-06-13 16:33:55 +0900

    Update goalyudo_nocamera.py

--- goalyudo_nocamera.py
+++ goalyudo_nocamera.py
@@ -57,7 +57,8 @@ try:
                                 lat = convert_to_decimal(parts[3], parts[4])
                                 lon = convert_to_decimal(parts[5], parts[6])
                                 print("緯度と経度 (10進数):", [lat, lon])
-                                global last_lng = [lat, lon]
+                                global last_lng
+                                last_lng = [lat, lon]
             except Exception as e:
                 print("デコードエラー:", e)
         time.sleep(0.1)
