818bea6b3ce7e60d75751c4008573e8de6f42d1d
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-13 12:50:35 +0900

    Update goal_detective_navigate.py

--- goal_detective_navigate.py
+++ goal_detective_navigate.py
@@ -43,6 +43,43 @@ def get_percentage():
     return percentage
 
 def get_block_number():
+    number = None
+    frame = picam2.capture_array()
+    frame = cv2.GaussianBlur(frame, (5, 5), 0)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_red1 = np.array([0, 30, 30])
+    upper_red1 = np.array([20, 255, 255])
+    lower_red2 = np.array([95, 30, 30])
+    upper_red2 = np.array([130, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask = cv2.bitwise_or(mask1, mask2)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        largest = max(contours, key=cv2.contourArea)
+        M = cv2.moments(largest)
+        if M["m00"] != 0:
+            cx = int(M["m10"] / M["m00"])  # x座標の重心
+            width = frame.shape[1]
+            w = width // 5  # 5分割幅
+            if cx < w:
+                number = 1
+            elif cx < 2 * w:
+                number = 2
+            elif cx < 3 * w:
+                number = 3
+            elif cx < 4 * w:
+                number = 4
+            else:
+                number = 5
+        else:
+            print("⚠️ 重心が計算できません")
+    else:
+        print("❌ 赤色物体が見つかりません")
+    return number
+                
+        
+
     
   
 try:
