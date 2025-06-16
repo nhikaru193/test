ca4e30f064cdc61eae58f8ea0390614f790c5ec9
garigari-hota <daoyuanfanang@gmail.com>
2025-06-16 13:29:29 +0900

    Update IM920SL_bbm.py

--- IM920SL_bbm.py
+++ IM920SL_bbm.py
@@ -5,7 +5,7 @@ im920 = serial.Serial('/dev/serial0', 19200, timeout=1)
 
 for i in range(10):
     data = f'TEST{i:02}'
-    msg = f'TXDA TEST{i:02}\r\n'
+    msg = f'TXDA 0003,{data}\r\n'
     im920.write(msg.encode())
     print(f"送信: {msg.strip()}")
     time.sleep(1)
