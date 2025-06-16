682149e001da75ba67bbcdac15ecc1694fe3fadd
garigari-hota <daoyuanfanang@gmail.com>
2025-06-16 20:36:24 +0900

    Update imtes.py

--- imtes.py
+++ imtes.py
@@ -1,3 +1,10 @@
-msg = f'TXDA 0011,HELLO,12345\r\n'
-im920.write(msg.encode())
-print(f"送信: {msg.strip()}")
+import serial
+import time
+
+im920 = serial.Serial('/dev/serial0', 19200, timeout=1)
+
+for i in range(10):
+    msg = f"HELLO {i}"
+    im920.write(msg.encode('ascii'))  # ASCII文字列を直接送信
+    print(f"送信: {msg}")
+    time.sleep(1)
