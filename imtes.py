456229fe3835c51798176651b6d1c001f24fc4cd
garigari-hota <daoyuanfanang@gmail.com>
2025-06-16 20:54:10 +0900

    Update imtes.py

--- imtes.py
+++ imtes.py
@@ -4,7 +4,8 @@ import time
 im920 = serial.Serial('/dev/serial0', 19200, timeout=1)
 
 for i in range(10):
-    msg = f"HELLO {i}"
-    im920.write(msg.encode('ascii'))  # ASCII文字列を直接送信
-    print(f"送信: {msg}")
+    msg = f"HELLO {i}\r\n"  # 改行コード追加
+    im920.write(msg.encode('ascii'))
+    print(f"送信: {msg.strip()}")
     time.sleep(1)
+
