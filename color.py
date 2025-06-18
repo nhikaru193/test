832a6d6d504e0519368fcf759afad82396b9a914
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-18 12:51:14 +0900

    Update color.py

--- color.py
+++ color.py
@@ -1,1 +1,67 @@
+import cv2
+import math
+import time
+from picamera2 import Picamera2
+import numpy as np
 
+#カメラ初期設定関数
+def init_camera():
+    picam2 = Picamera2()
+    config = picam2.create_still_configuration(main={"size": (320, 240)})
+    picam2.configure(config)
+    picam2.start()
+    time.sleep(1)
+  
+#関数定義(画像取得→画像処理→hsv変換→面積計算→割合計算)　戻り値はpercentage(画像赤色検知割合)
+def get_percentage():
+    frame = picam2.capture_array()
+    frame = cv2.GaussianBlur(frame, (5, 5), 0)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_red1 = np.array([0, 30, 30])
+    upper_red1 = np.array([20, 255, 255])
+    lower_red2 = np.array([95, 30, 30])
+    upper_red2 = np.array([130, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask = cv2.bitwise_or(mask1, mask2)
+    red_area = np.count_nonzero(mask)
+    total_area = frame.shape[0] * frame.shape[1]
+    percentage = (red_area / total_area) * 100
+    return percentage
+
+#赤色面積の重心がどこにあたるか
+def get_block_number():
+    number = None
+    frame = picam2.capture_array()
+    frame = cv2.GaussianBlur(frame, (5, 5), 0)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_red1 = np.array([0, 30, 30])
+    upper_red1 = np.array([20, 255, 255])
+    lower_red2 = np.array([95, 30, 30])
+    upper_red2 = np.array([130, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask = cv2.bitwise_or(mask1, mask2)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        largest = max(contours, key=cv2.contourArea)
+        M = cv2.moments(largest)
+        if M["m00"] != 0:
+            cx = int(M["m10"] / M["m00"])  # x座標の重心
+            width = frame.shape[1]
+            w = width // 5  # 5分割幅
+            if cx < w:
+                number = 1
+            elif cx < 2 * w:
+                number = 2
+            elif cx < 3 * w:
+                number = 3
+            elif cx < 4 * w:
+                number = 4
+            else:
+                number = 5
+        else:
+            print("⚠️ 重心が計算できません")
+    else:
+        print("❌ 赤色物体が見つかりません")
+    return number
