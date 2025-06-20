daf041e666213864374ab3555b32e7b89b898a21
garigari-hota <daoyuanfanang@gmail.com>
2025-06-20 14:22:52 +0900

    Update IM920SL_bbm.py

--- IM920SL_bbm.py
+++ IM920SL_bbm.py
@@ -5,8 +5,8 @@ im920 = serial.Serial('/dev/serial0', 19200, timeout=1)
 
 for i in range(10):
     data = f'TEST{i:02}'                 # → 6文字
-    length = len(data)                   # → 6
-    msg = f'TXDA {length:04},{data}\r\n' # → 'TXDA 0006,TEST00\r\n'
+    node = "0003"                        # 送信先ノード番号
+    msg = f'TXDU {node},{data}\r\n'      # ユニキャスト送信コマンド
     im920.write(msg.encode())
     print(f"送信: {msg.strip()}")
     time.sleep(1)
