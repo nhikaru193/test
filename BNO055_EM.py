dc378a85af4f9cac109130e211239875372a36e3
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-23 12:37:57 +0900

    Create BNO055_EM.py

---
+++ BNO055_EM.py
@@ -0,0 +1,14 @@
+import smbus
+import time
+import struct
+
+bno = BNO055()
+if bno.begin() is not True:
+    print("Error initializing device")
+    exit()
+    time.sleep(1)
+bno.setMode(BNO055.OPERATION_MODE_NDOF)
+bno.setExternalCrystalUse(True)
+time.sleep(1)
+heading = bno.get_heading()
+print(heading)
