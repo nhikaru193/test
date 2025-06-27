d4b48ccec7f2da747d9b6ff880c58fc8e44ab9bf
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-27 15:51:31 +0900

    Update BME280_MOTOR.py

--- BME280_MOTOR.py
+++ BME280_MOTOR.py
@@ -175,14 +175,14 @@ init_bme280()
 #補正データ読み込み
 read_compensate()
 
-driver.changing_forward(0, 50)
-time.sleep(60)
+driver.changing_forward(0, 100)
+time.sleep(5)
 #測定データ読み込み 
 for i in range (100):
 	read_data()
 	time.sleep(0.01)  # 0.01秒待機
 
-driver.changing_forward(50, 0)
+driver.changing_forward(100, 0)
 time.sleep(5)
 GPIO.cleanup()
 print("GPIOのクリーンアップを実行しました。")
