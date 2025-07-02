e1a7359e83a8cf06148814e2cb811463a029178a
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-02 20:10:42 +0900

    Update servomotor_bbm.py

--- servomotor_bbm.py
+++ servomotor_bbm.py
@@ -33,9 +33,9 @@ try:
     
     """
     print("逆回転（速い）")
-    set_servo_duty(2.5)
-    time.sleep(3)
-    set_servo_duty(12.5)
+    set_servo_duty(1.0)
+    time.sleep(10)
+    set_servo_duty(14.0)
     time.sleep(3)
     """
 
