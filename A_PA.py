5837d43661c3f700a35408633531ec58db6b97cf
garigari-hota <daoyuanfanang@gmail.com>
2025-09-12 04:31:50 +0900

    Update A_PA.py

--- A_PA.py
+++ A_PA.py
@@ -4,7 +4,7 @@ import time
 import serial
 import pigpio
 import RPi.GPIO as GPIO
-from A_MD import MotorDriver      # ユーザーのMotorDriverクラスを使用
+from A_Motor import MotorDriver      # ユーザーのMotorDriverクラスを使用
 from A_BNO055 import BNO055
 import smbus
 import struct
